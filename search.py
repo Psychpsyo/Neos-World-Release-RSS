00e5de7ae134c4bcf09d378b3ee43dd25174d3ab
Psychpsyo <psychpsyo@gmail.com>
2023-04-08 14:41:24 +0200

    Add featured worlds feed

--- search.py
+++ search.py
@@ -10,15 +10,17 @@ NEOS_API = "https://cloudx.azurewebsites.net/"
 NEOS_ASSET_ENDPOINT = "https://assets.neos.com/assets/"
 FEED_LINK = "https://feed.neos.love/atom.xml"
 feed_cache = {}
+featured_feed_cache = {}
 
 #raised by the server code to have it return 400 instead of 500.
 class ClientException(Exception):
 	pass
 
-def getFeed(page = 1):
+def getFeed(page = 1, featuredOnly = False):
+	cache = (featuredOnly? featured_feed_cache : feed_cache)
 	# if page was cached within the last 30 minutes
-	if page in feed_cache and (datetime.datetime.now() - feed_cache[page]["lastUpdated"]).total_seconds() < 60 * 30:
-		return feed_cache[page]["feedString"]
+	if page in cache and (datetime.datetime.now() - cache[page]["lastUpdated"]).total_seconds() < 60 * 30:
+		return cache[page]["feedString"]
 	
 	worldResponse = requests.post(
 		NEOS_API + "api/records/pagedSearch",
@@ -26,6 +28,7 @@ def getFeed(page = 1):
 			"count": 50,
 			"offset": (page - 1) * 50,
 			"private": False,
+			"onlyFeatured": featuredOnly,
 			"recordType": "world",
 			"sortBy": "FirstPublishTime",
 			"sortDirection": "Descending"
@@ -35,7 +38,7 @@ def getFeed(page = 1):
 	# On Neos server error, serve from cache if you can
 	if worldResponse.status_code != 200:
 		print("Failed to request updated Neos sessions!")
-		return feed_cache.get(page, {"feedString": ""})["feedString"]
+		return cache.get(page, {"feedString": ""})["feedString"]
 	
 	# otherwise regenerate the feed
 	
@@ -44,12 +47,15 @@ def getFeed(page = 1):
 	xmlString = ""
 	
 	with minidom.parse("templateFeed.xml") as atomFeed:
+		atomFeed.getElementsByTagName("title").item(0).firstChild.data = featuredOnly? "Featured Neos Worlds" : "Published Neos Worlds"
+		atomFeed.getElementsByTagName("id").item(0).firstChild.data = featuredOnly? "https://feed.neos.love/atom.xml?featuredOnly=true" : "https://feed.neos.love/atom.xml"
+
 		atomFeed.getElementsByTagName("updated").item(0).appendChild(atomFeed.createTextNode(atomNow))
 		feed = atomFeed.getElementsByTagName("feed").item(0)
 
 		linkSelf = atomFeed.createElement("link")
 		linkSelf.setAttribute("rel", "self")
-		linkSelf.appendChild(atomFeed.createTextNode(FEED_LINK + ("/?page=" + str(page) if page > 1 else "")))
+		linkSelf.appendChild(atomFeed.createTextNode(FEED_LINK + ("?page=" + str(page) if page > 1 else "")))
 		feed.appendChild(linkSelf)
 
 		linkFirst = atomFeed.createElement("link")
@@ -60,12 +66,12 @@ def getFeed(page = 1):
 		if page > 1:
 			linkPrev = atomFeed.createElement("link")
 			linkPrev.setAttribute("rel", "previous")
-			linkPrev.appendChild(atomFeed.createTextNode(FEED_LINK + "/?page=" + str(page - 1)))
+			linkPrev.appendChild(atomFeed.createTextNode(FEED_LINK + "?page=" + str(page - 1)))
 			feed.appendChild(linkPrev)
 
 		linkNext = atomFeed.createElement("link")
 		linkNext.setAttribute("rel", "next")
-		linkNext.appendChild(atomFeed.createTextNode(FEED_LINK + "/?page=" + str(page + 1)))
+		linkNext.appendChild(atomFeed.createTextNode(FEED_LINK + "?page=" + str(page + 1)))
 		feed.appendChild(linkNext)
 		
 		for world in worldList:
@@ -142,11 +148,11 @@ def getFeed(page = 1):
 		xmlString = atomFeed.toprettyxml(encoding="utf-8", standalone=True).decode("utf-8")
 		xmlString = "\n".join([line for line in xmlString.splitlines() if line.strip()])
 	
-	feed_cache[page] = {
+	cache[page] = {
 		"lastUpdated": datetime.datetime.now(),
 		"feedString": xmlString
 	}
-	return feed_cache[page]["feedString"]
+	return cache[page]["feedString"]
 
 
 
@@ -155,11 +161,12 @@ class HttpHandler(BaseHTTPRequestHandler):
 		try:
 			query = parse_qs(self.path[2:])
 			page = int(query.get("page", ["1"])[0])
+			featuredOnly = query.get("featuredOnly", ["false"])[0] == "true"
 
 			if (page < 1):
 				raise ClientException()
 
-			feed = getFeed(page)
+			feed = getFeed(page, featuredOnly)
 			if feed == "":
 				self.send_response(404)
 				self.send_header("Access-Control-Allow-Origin", "*")
